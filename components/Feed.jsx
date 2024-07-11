756721f70e61721c7cad31a85f4ada256554e8ce
youneszh1 <younes.zh2016@gmail.com>
2024-07-11 13:03:10 +0100

    first commit

---
+++ components/Feed.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useEffect, useState } from 'react'
+import PromptCard from './PromtCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post.id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/prompt');
+      const data = await res.json();
+
+      setPosts(data)
+    }
+    fetchPosts()
+  }, []);
+  return (
+    <section className='feed'>
+      <form action="" className="relative w-full flex-center">
+        <input type="text" placeholder='Search for a tag or a username' value={searchText} onChange={handleSearchChange} required className='form_input peer' />
+      </form>
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
+  )
+}
+
+export default Feed
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} 
 const nextConfig = {};
+
+export default nextConfig;
+*/
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                pathname: "**",
+            },
+        ],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    },
+};
 
 export default nextConfig;
